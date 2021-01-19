61019ae08e5412aae58121b0201c9504991ebdd7
fechan <edu.fechan@gmail.com>
2021-01-18 23:06:46 -0800

    add excludes

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pysubs2
 from fugashi import Tagger
 from jamdict import Jamdict
 
+SUBTITLE_FILE = "test_anime.ass"
+POS_EXCLUDES = ["助詞", "助動詞", "空白"] # parts of speech to exclude
+
 tagger = Tagger("-Owakati")
 jmd = Jamdict()
 
-subs = pysubs2.load("test_anime.ass")
+subs = pysubs2.load(SUBTITLE_FILE)
 
-text = subs[50].plaintext
-for word in tagger(text):
-    definition = jmd.lookup(word.feature.lemma)
-    print(definition.entries[0].senses[0])
-    print(word, word.feature.lemma, sep="\t")
-
+for event in subs:
+    if event.style == "JP": # TODO: this is subtitle specific; should make a more flexible filter
+        text = event.plaintext
+        if text != "":
+            for word in tagger(text):
+                try:
+                    if word.feature.pos1 not in POS_EXCLUDES:
+                        definition = jmd.lookup(word.feature.lemma)
+                        print(word, word.feature.pos1, definition.entries[0].senses[0], sep="\t")
+                except:
+                    print("Failed to get definition of", word)
