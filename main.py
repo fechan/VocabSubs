194eb6322ae36b6c90be8aaa02bd955b264c7b02
fechan <edu.fechan@gmail.com>
2021-01-20 18:40:28 -0800

    write definitions to subtitle file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fugashi import Tagger
 from jamdict import Jamdict
 
 SUBTITLE_FILE = "test_anime.ass"
+OUTPUT_FILE = "test_anime_out.ass"
 POS_EXCLUDES = ["助詞", "助動詞", "空白"] # parts of speech to exclude
 
 tagger = Tagger("-Owakati")
@@ -18,9 +19,11 @@ for event in subs:
         text = event.plaintext
         if text != "":
             for word in tagger(text):
-                try:
-                    if word.feature.pos1 not in POS_EXCLUDES:
+                if word.feature.pos1 not in POS_EXCLUDES:
+                    try:
                         definition = jmd.lookup(word.feature.lemma)
-                        print(word, word.feature.pos1, definition.entries[0].senses[0], sep="\t")
-                except:
-                    print("Failed to get definition of", word)
+                        new_text = f"{word}\t{definition.entries[0].senses[0]}"
+                        subs.insert(-1, pysubs2.SSAEvent(start=event.start, end=event.end, text=new_text))
+                    except (IndexError, ValueError):
+                        print("Failed to get definition of", word)
+subs.save(OUTPUT_FILE)
